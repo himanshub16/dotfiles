52b4fe09dd77e4c0ed5d02d9346d09b6f484d86b
Himanshu Shekhar <himanshushekharb16@gmail.com>
2016-09-19 02:30:32 +0530

    fixed bug in netcheck which replaced any existing file

--- net_check.sh
+++ net_check.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Author : Himanshu Shekhar < https://github.com/himanshub16 >
 # script to check whether the system is connected to the internet
-wget -t 2 --quiet www.google.co.in 
-if [ -f 'index.html' ]; then
+outputfile="tryingtohaveanamewhichdoesntconflictwithanyexistingfile"
+
+wget -t 2 --quiet www.google.co.in -O $outputfile
+if [ -f $outputfile ]; then
 	echo "network is up"
-	rm 'index.html'
+	rm $outputfile
 else
 	echo "network is down"
 fi
