7e53800be2b18ef23bad3780d6f16ddd3833fc36
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-01-25 20:30:24 +0530

    added script to update time

---
+++ update-time.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Update system time from ...
+# Google, not NTP servers
+
+# Source: http://askubuntu.com/questions/81293/what-is-the-command-to-update-time-and-date-from-internet 
+
+sudo date -s "$(wget -qSO- --max-redirect=0 google.com 2>&1 | grep Date: | cut -d' ' -f5-8)Z"
+
+echo "Done"
