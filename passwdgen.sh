f72ca415fee141fd382607c8f746a4eea7712edf
Himanshu Shekhar <himanshushekharb16@gmail.com>
2017-02-15 21:54:04 +0530

    useless commit

--- passwdgen.sh
+++ passwdgen.sh
@@ -1,10 +1,11 @@
+#!/bin/sh
 # Shell script to generate random passwords
 # and copy it to clipboard
 # for my personal use
 
 password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9-_!@#$%^&*()_+{}|:<>?=' | fold -w 10 | grep -i '[!@#$%^&*()_+{}|:<>?=]' | head -n 1 ; echo -n)
 
-echo "Password : " $password 
+echo "Password : " $password
 echo -n $password | xclip -selection clipboard
 
 echo "Password copied to clipboard."
